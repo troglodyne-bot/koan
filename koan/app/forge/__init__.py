b505b1dceedee1a2bc48004a57ecac8850ee8dea
George Baugh <george@troglodyne.net>
2026-06-03 18:44:19 +0000

    forgot netloc was an attr, not a method

--- koan/app/forge/__init__.py
+++ koan/app/forge/__init__.py
@@ -74,7 +74,7 @@ def detect_forge_from_url(url: str) -> ForgeProvider:
     from urllib.parse import urlparse
     parsed=urlparse(lower);
 
-    netloc = parsed.netloc();
+    netloc = parsed.netloc;
 
     # While this still allows for nefarious github.enterprise.whatever,
     # we presume that is intentional subdomain design in that case
